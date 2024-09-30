30340e7a77a7c8607045c024ff9fa1acdb78ef75
pablo-jose-f <silva.pablo21@escola.pr.gov.br>
2024-09-30 11:39:12 -0300

    30/9

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -3,11 +3,12 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const paragrafo = document.createElement ('p')
-    paragrafo.classList.add ('graficos-container_texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dado.total_pessoas_conectadas} estão conectadas em algum rede social e paam em média ${dados.tempo_medio} horas conectadas.`
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
     console.log(paragrafo);
-    }
-visualizarInformacoesGlobais() 
+    
+}
+visualizarInformacoesGlobais()
