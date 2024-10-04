cabbc5e058385b9b36572db31e2595f4b2c8541c
pablo-jose-f <silva.pablo21@escola.pr.gov.br>
2024-10-04 09:18:09 -0300

    horas

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -3,12 +3,14 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
+    const pessoasConetadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
+    const horas = parseInt(dados.tempo_medio)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${dados.total_pessoas_mundo} </span> de pessoas e que aproximadamente <span> ${dados.total_pessoas_conectadas} </span> estão conectadas em alguma rede social e passam em média <span> ${dados.tempo_medio} </span> horas conectadas.`
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span> ${pessoasNoMundo} bilhões </span> de pessoas e que aproximadamente <span> ${pessoasConectadas} Bilhões </span> estão conectadas em alguma rede social e passam em média <span> ${dados.tempo_medio} </span> horas conectadas.`
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
-    console.log(paragrafo);
     
 }
 visualizarInformacoesGlobais()
